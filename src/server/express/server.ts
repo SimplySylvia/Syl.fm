f6d63d5736617b6ad467c7211912fb3eb2e9b286
SimplySylvia <sylvia.the.dev@gmail.com>
2024-06-13 15:18:27 -0700

    :ambulance: (SERVER) port correction for prod env

--- src/server/express/server.ts
+++ src/server/express/server.ts
@@ -3,10 +3,12 @@ dotenv.config();
 import { createServer } from "http";
 import apiV1 from "./V1";
 
+const PORT = process.env.VITE_BACKEND_PORT || process.env.PORT;
+
 const server = createServer();
 
 server.on("request", apiV1);
 
-server.listen(process.env.VITE_BACKEND_PORT, () => {
+server.listen(PORT, () => {
   console.log(`API v1 (re)started`);
 });
